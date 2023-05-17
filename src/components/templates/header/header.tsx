77e343a452a02b55bb96326627b2142d084f6bb0
davidwilkes-avionos <david.wilkes@avionos.com>
2023-05-17 12:31:13 -0400

    Logo Favicon Updates
    Update header for Logos for AVI Mfg
    Added AVI Mfg Images

--- src/components/templates/header/header.tsx
+++ src/components/templates/header/header.tsx
@@ -2,8 +2,8 @@ import { Box, BoxProps, Flex } from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
 
-import MobileLogo from '@icons/bonelli-mobile.svg';
-import DesktopLogo from '@icons/bonelli.svg';
+import MobileLogo from '@icons/android-chrome-512x512.png';
+import DesktopLogo from '@icons/2.png';
 import { LanguageSelector } from '@src/components/features/language-selector';
 
 export const HEADER_HEIGHT = 60;
